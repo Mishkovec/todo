38de5c2eaf3de5404cc0e0d249131fe55612f4b8
Mishkovec <neilsa@mail.ru>
2019-11-17 21:08:00 +0300

    feature: create basic layout

---
+++ todo/src/components/todo-list-item.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './todo-list-item.css'
+
+const TodoListItem = ({label, important=false}) => {
+    const style = {
+        color: important ? 'tomato' : 'black'
+    };
+    return(
+        <span className='todo-list-item'>
+            <span 
+                style={style}
+                className='todo-list-item-label'
+            >
+                {label}
+            </span>
+            <div>
+                <button
+                    type='button'
+                    className='btn btn-outline-success'
+                >
+                    <i className='fa fa-exclamation'/>
+                </button>
+                <button
+                    type='button'
+                    className='btn btn-outline-danger'
+                >
+                    <i className='fa fa-trash-o'/>
+                </button>
+            </div>
+        </span>
+    );
+};
+
+export default TodoListItem;
